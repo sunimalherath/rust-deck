7272ae3880831eaa4b44e4db3cf764545f1bccc2
Sunimal Herath <sunimaldv@gmail.com>
2024-07-27 22:19:48 +1000

    adding struct for deck

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#[derive(Debug)] // derive attribute for Deck to implement Debug trait.
+struct Deck {
+    cards: Vec<String>,
+}
 fn main() {
-    println!("Hello, world!");
+    // let deck = Deck { cards: Vec::new()}; // another way.
+    let deck = Deck { cards: vec![]};
+
+    println!("Here's your deck:\n {:#?}", deck);
 }
