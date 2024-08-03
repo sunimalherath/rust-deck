e2bf2e795d989d679099493fc240029487d26d89
Sunimal Herath <sunimaldv@gmail.com>
2024-08-03 11:50:12 +1000

    adding inherent implementation to Deck

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,35 @@
 struct Deck {
     cards: Vec<String>,
 }
-fn main() {
-    let suits = ["Hearts", "Spades", "Clubs", "Diamonds"];
-    let values = [
-        "Two", "Three", "Four", "Five","Six", "Seven", "Eight",
-        "Nine", "Ten", "Jack", "Queen", "King", "Ace"
-    ];
 
-    let mut cards = vec![];
+// adding inherent implementation
+impl Deck {
+    // fn new() -> Deck
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Clubs", "Diamonds"];
+        let values = [
+            "Two", "Three", "Four", "Five","Six", "Seven", "Eight",
+            "Nine", "Ten", "Jack", "Queen", "King", "Ace"
+        ];
 
-    for suit in suits {
-        for value in values {
-            let card = format!("{} of {}", value, suit);
+        let mut cards = vec![];
+
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
                 cards.push(card);
+            }
         }
-    }
 
-    // let deck = Deck { cards: Vec::new()}; // another way.
-    // let deck = Deck { cards: cards}; // This can be simplified as follows
-    let deck = Deck { cards };
+        // let deck = Deck { cards: Vec::new()}; // another way.
+        // let deck = Deck { cards: cards}; // This can be simplified as follows
+        let deck = Deck { cards };
+
+        return deck;
+    }
+}
+fn main() {
+    let deck = Deck::new();
 
     println!("Here's your deck:\n {:#?}", deck);
 }
