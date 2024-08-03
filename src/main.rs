3b090c62ed1e8a33cb74b9ea6053f74fe5182071
Sunimal Herath <sunimaldv@gmail.com>
2024-08-03 11:39:22 +1000

    populating the deck with cards

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,24 @@ struct Deck {
     cards: Vec<String>,
 }
 fn main() {
+    let suits = ["Hearts", "Spades", "Clubs", "Diamonds"];
+    let values = [
+        "Two", "Three", "Four", "Five","Six", "Seven", "Eight",
+        "Nine", "Ten", "Jack", "Queen", "King", "Ace"
+    ];
+
+    let mut cards = vec![];
+
+    for suit in suits {
+        for value in values {
+            let card = format!("{} of {}", value, suit);
+                cards.push(card);
+        }
+    }
+
     // let deck = Deck { cards: Vec::new()}; // another way.
-    let deck = Deck { cards: vec![]};
+    // let deck = Deck { cards: cards}; // This can be simplified as follows
+    let deck = Deck { cards };
 
     println!("Here's your deck:\n {:#?}", deck);
 }
