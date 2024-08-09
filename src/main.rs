f211fe4fdb3152282451f5b2a884a7b194c78b37
Sunimal Herath <sunimal@live.com>
2024-08-09 22:23:15 +1000

    implementing deal method

--- src/main.rs
+++ src/main.rs
@@ -38,11 +38,18 @@ impl Deck {
         let mut rng = thread_rng(); // random number generator
         self.cards.shuffle(&mut rng);
     }
+
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
+    }
 }
 fn main() {
     let mut deck = Deck::new();
 
     deck.shuffle();
 
+    let cards = deck.deal(3);
+
     println!("Here's your deck:\n {:#?}", deck);
+    println!("Here's your hand:\n {:#?}", cards)
 }
